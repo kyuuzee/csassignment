745290aff27c842df80585eb85988530bd6ea37c
kyuuzee <60009410+kyuuzee@users.noreply.github.com>
2020-01-20 15:52:29 +0800

    updated list

--- hashD.sh
+++ hashD.sh
@@ -6,7 +6,6 @@ LIST1="A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
 LIST2="0 1 2 3 4 5 6 7 8 9"
 LIST3="a b c d e f g h i j k l m n o p q r s t u v w x y z"
 LIST4="~ @ # $ % ^ * _ + - = { } [ ] ? ; :"
-LIST5="~ @ # $ % ^ * _ + - = { } [ ] ? ; :"T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z ~ @ # $ % ^ * _ - + = [ ] { } \  ; :  . , / ? 0 1 2 3 4 5 6 7 8 9"
 LIST5="~ @ # $ % ^ * _ + - = { } [ ] ? ; :"
 
 
